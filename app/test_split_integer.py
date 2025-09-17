d7f5880b7b9ffd43dcbdd86de5ecac020bb59c8b
Radoslaw Ptak <radek1026@interi.pl>
2025-09-17 14:39:53 +0200

    Changes in implement py-split-integer

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -3,34 +3,29 @@ from app.split_integer import split_integer
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
 
-    assert (sum(split_integer(8, 1)) == 8)
+    assert split_integer(8, 1) == [8]
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
-    assert (split_integer(6, 2) == [3, 3])
+    assert split_integer(6, 2) == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
-    assert (split_integer(6, 1) == [6])
+    assert split_integer(6, 1) == [6]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(32, 6)
-    assert result == sorted(result)
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
 
-    assert (split_integer(3, 4) == [0, 1, 1, 1])
+    assert split_integer(3, 4) == [0, 1, 1, 1]
 
 
 def test_difference_between_max_and_min_should_be_at_most_one() -> None:
-    result = split_integer(17, 4)
-    assert max(result) - min(result) <= 1
-
-
-def test_test_should_return_exact_number_of_parts() -> None:
-    result = split_integer(8, 4)
-    assert len(result) == 4
+    assert split_integer(17, 4) == [4, 4, 4, 5]
+    assert len(split_integer(17, 4)) == 4
+    assert sum(split_integer(17, 4)) == 17
